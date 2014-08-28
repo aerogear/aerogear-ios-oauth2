8bb266ca5034e987ed1dcc2ea642d1c9fae5b5b4
corinnekrych <corinnekrych@gmail.com>
2014-08-28 10:16:59 +0200

    initial project

---
+++ AeroGearOAuth2/AeroGearOAuth2.h
@@ -0,0 +1,19 @@
+//
+//  AeroGearOAuth2.h
+//  AeroGearOAuth2
+//
+//  Created by Corinne Krych on 28/08/14.
+//  Copyright (c) 2014 aerogear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AeroGearOAuth2.
+FOUNDATION_EXPORT double AeroGearOAuth2VersionNumber;
+
+//! Project version string for AeroGearOAuth2.
+FOUNDATION_EXPORT const unsigned char AeroGearOAuth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AeroGearOAuth2/PublicHeader.h>
+
+
