42713b9974ab55f154df8cfc4551d607d1ae1636
Michael Doo <michael@410labs.com>
2015-12-01 14:36:50 -0500

    Add audience field and server code to OAuth2Module
    This commit allows developers to do two things:
    
    * Ensure that the app making the OAuth2 request is the intended recipient of the
    access token (see here for more: https://oauth.net/articles/authentication/) by
    validating the audience field against the client ID
    
    * Allow developers to do cross-client authorization, where both a client and backend
    server can make separate authorized requests to a service without each needing
    to be authorized by the user. See more info here:
    https://developers.google.com/identity/protocols/CrossClientAuth and here:
    https://developers.google.com/identity/sign-in/ios/offline-access

--- AeroGearOAuth2/OAuth2Module.swift
+++ AeroGearOAuth2/OAuth2Module.swift
@@ -57,6 +57,7 @@ open class OAuth2Module: AuthzModule {
     var state: AuthorizationState
     open var webView: OAuth2WebViewController?
     open var idToken: String?
+    open var serverCode: String?
 
     /**
     Initialize an OAuth2 module.
@@ -122,13 +123,18 @@ open class OAuth2Module: AuthzModule {
         self.state = .authorizationStatePendingExternalApproval
 
         // calculate final url
-        let params = "?scope=\(config.scope)&redirect_uri=\(config.redirectURL.urlEncode())&client_id=\(config.clientId)&response_type=code"
-        guard let computedUrl = http.calculateURL(baseURL: config.baseURL, url:config.authzEndpoint) else {
+        var params = "?scope=\(config.scope)&redirect_uri=\(config.redirectURL.urlEncode())&client_id=\(config.clientId)&response_type=code"
+
+        if let audienceId = config.audienceId {
+            params = "\(params)&audience=\(audienceId)"
+        }
+
+        guard let computedUrl = http.calculateURL(baseURL: config.baseURL, url: config.authzEndpoint) else {
             let error = NSError(domain:AGAuthzErrorDomain, code:0, userInfo:["NSLocalizedDescriptionKey": "Malformatted URL."])
             completionHandler(nil, error)
             return
         }
-        if let url = URL(string:computedUrl.absoluteString + params) {
+        if let url = URL(string: computedUrl.absoluteString + params) {
             if self.webView != nil {
                 self.webView!.targetURL = url
                 config.webViewHandler(self.webView!, completionHandler)
@@ -186,6 +192,10 @@ open class OAuth2Module: AuthzModule {
             paramDict["client_secret"] = unwrapped
         }
 
+        if let audience = config.audienceId {
+            paramDict["audience"] = audience
+        }
+
         http.request(method: .post, path: config.accessTokenEndpoint, parameters: paramDict as [String : AnyObject]?, completionHandler: {(responseObject, error) in
             if (error != nil) {
                 completionHandler(nil, error)
@@ -203,6 +213,7 @@ open class OAuth2Module: AuthzModule {
         let accessToken: String   = unwrappedResponse["access_token"] as! String
         let refreshToken: String? = unwrappedResponse["refresh_token"] as? String
         let idToken: String?      = unwrappedResponse["id_token"] as? String
+        let serverCode: String?   = unwrappedResponse["server_code"] as? String
         let expiration            = unwrappedResponse["expires_in"] as? NSNumber
         let exp: String?          = expiration?.stringValue
         // expiration for refresh token is used in Keycloak
@@ -211,6 +222,7 @@ open class OAuth2Module: AuthzModule {
 
         self.oauth2Session.save(accessToken: accessToken, refreshToken: refreshToken, accessTokenExpiration: exp, refreshTokenExpiration: expRefresh, idToken: idToken)
         self.idToken    = self.oauth2Session.idToken
+        self.serverCode = serverCode
 
         return accessToken
     }
