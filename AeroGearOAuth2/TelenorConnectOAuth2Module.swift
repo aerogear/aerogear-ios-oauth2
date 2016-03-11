5819a7a2071dfa66865c4d547534a5069a700b79
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-03-11 15:05:38 +0100

    Add incomplete TelenorConnectOAuth2Module

---
+++ AeroGearOAuth2/TelenorConnectOAuth2Module.swift
@@ -0,0 +1,31 @@
+//
+//  TelenorConnectOAuth2Module.swift
+//  Pods
+//
+//  Created by Jørund Fagerjord on 11/03/16.
+//
+//
+
+import Foundation
+
+import AeroGearHttp
+
+public class TelenorConnectOAuth2Module: OAuth2Module {
+    
+    override public func revokeAccess(completionHandler: (AnyObject?, NSError?) -> Void) {
+        // TODO: also revoke refreshToken
+        if (self.oauth2Session.accessToken == nil) {
+            return;
+        }
+        let paramDict:[String:String] = [ "client_id": config.clientId, "token": self.oauth2Session.accessToken!]
+        http.request(.POST, path: config.revokeTokenEndpoint!, parameters: paramDict, responseSerializer: StringResponseSerializer(), completionHandler: { (response, error) in
+            if (error != nil) {
+                completionHandler(nil, error)
+                return
+            }
+            
+            self.oauth2Session.clearTokens()
+            completionHandler(response, nil)
+        })
+    }
+}
