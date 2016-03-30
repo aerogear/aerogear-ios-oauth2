88032f1de855d3a6fe7602f5c59173c274a1f858
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2016-03-30 12:32:43 +0200

    Add IdTokenValidatorTest

---
+++ AeroGearOAuth2Tests/IdTokenValidatorTest.swift
@@ -0,0 +1,101 @@
+//
+//  IdTokenValidatorTest.swift
+//  AeroGearOAuth2
+//
+//  Created by Jørund Fagerjord on 30/03/16.
+//  Copyright © 2016 aerogear. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+import AeroGearOAuth2
+
+class IdTokenValidatorTest: XCTestCase {
+    
+    func testMissingIssuerReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["aud": ["expectedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testUnmatchingIssuerReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuerAlmost", "aud": ["expectedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testMissingAudienceReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testNotContainingAudienceReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": [], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testUntrustedAudiencePresentReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["untrustedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testMoreThanOneAudienceButNoAuthorizedPartyReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience", "untrustedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testMoreThanOneAudienceAndUnmatchingAuthorizedPartyReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience", "someAudience"], "azp": "someAudience", "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testMissingExperationTimeReturnsError() {
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience"], "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testExpiredExperationTimeReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 - 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testMissingIssueTimeReturnsError() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience"], "exp": experationTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNotNil(error)
+    }
+    
+    func testEverythingValidReturnsNils() {
+        let experationTime: String = String(NSDate().timeIntervalSince1970 + 100)
+        let issueTime: String = String(NSDate().timeIntervalSince1970)
+        let token: [String: AnyObject] = ["iss": "expectedIssuer", "aud": ["expectedAudience"], "exp": experationTime, "iat": issueTime]
+        let error: IdTokenValidationError? = validateIdToken(token, expectedIssuer: "expectedIssuer", expectedAudience: "expectedAudience")
+        XCTAssertNil(error)
+    }
+}
