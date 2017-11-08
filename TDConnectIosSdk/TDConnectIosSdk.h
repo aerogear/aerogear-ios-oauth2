2787e598893ad617024e7e8b44c5627b1b8ebc75
Atanas Bakalov <atanas.bakalov@telenordigital.com>
2017-11-08 21:44:07 +0100

    Add modulemap and tweak podspec

--- TDConnectIosSdk/TDConnectIosSdk.h
+++ TDConnectIosSdk/TDConnectIosSdk.h
@@ -1,29 +1,16 @@
-/*
- * JBoss, Home of Professional Open Source.
- * Copyright Red Hat, Inc., and individual contributors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
 #import "ForcedHEManager.h"
 
-//! Project version number for TDConnectIosSdk.
-FOUNDATION_EXPORT double TDConnectIosSdkVersionNumber;
-
-//! Project version string for TDConnectIosSdk.
-FOUNDATION_EXPORT const unsigned char TDConnectIosSdkVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <TDConnectIosSdk/PublicHeader.h>
-
-
+FOUNDATION_EXPORT double AeroGearHttpVersionNumber;
+FOUNDATION_EXPORT const unsigned char AeroGearHttpVersionString[];
