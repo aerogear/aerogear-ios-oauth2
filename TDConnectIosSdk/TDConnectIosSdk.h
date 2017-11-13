48217e01f85d0970020b562df43bd15f0176f4c6
Atanas Bakalov <atanas.bakalov@telenordigital.com>
2017-11-13 16:53:49 +0100

    Fix __warning__ atribute for clang

--- TDConnectIosSdk/TDConnectIosSdk.h
+++ TDConnectIosSdk/TDConnectIosSdk.h
@@ -1,23 +1,2 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
-
-#import "ForcedHEManager.h"
-#import "curl.h"
-#import "curlver.h"
-#import "easy.h"
-#import "mprintf.h"
-#import "multi.h"
-#import "stdcheaders.h"
-#import "system.h"
-
-FOUNDATION_EXPORT double AeroGearHttpVersionNumber;
-FOUNDATION_EXPORT const unsigned char AeroGearHttpVersionString[];
+// Fix __warning__ atribute for clang
+#define __warning__ deprecated
