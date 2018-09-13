aerogear/aerogear-ios-oauth2

commit eabfed97dfa7240733078c252ac9827f2b420637

Refactor .well-known configuration retrieval (#44)
Refactor .well-known configuration retrieval to be made from Swift and
decouple it from ForcedHE code, so that the configuration could be used
for other purposes besides ForcedHE.

TDConnectIosSdk/OAuth2Module.swift
@@ -104,6 +104,7 @@ open class OAuth2Module: NSObject, AuthzModule, SFSafariViewControllerDelegate {
     var state: AuthorizationState
     var browserType: BrowserType
     var authenticationSession: Any? // We need this optional on the object otherwise the popup dialog disappears immediately. It has to be an Any instead of a SFAuthenticationSession because SFAuthenticationSession is only available in iOS 11+ and we do not want to mark the whole class with `@available(iOS 11.0, *)` and we can't use that syntax on stored properties.
+    var urlsForHE: [String]?
 
     /**
     Initialize an OAuth2 module.
@@ -131,13 +132,28 @@ open class OAuth2Module: NSObject, AuthzModule, SFSafariViewControllerDelegate {
             config.optionalParams = [String: String]();
         }
 
-        ForcedHEManager.initForcedHE(config.wellKnownConfigurationEndpoint);
-
         self.config = config
         
         self.http = Http(baseURL: config.baseURL, requestSerializer: requestSerializer, responseSerializer:  responseSerializer)
         self.state = .authorizationStateUnknown
         self.browserType = .unknown;
+
+        super.init()
+
+        ForcedHEManager.initForcedHE()
+        self.fetchWellknownConfig()
+    }
+
+    func fetchWellknownConfig() {
+        self.http.request(method: .get, path: config.wellKnownConfigurationEndpoint!, completionHandler: { (response, error) in
+            guard let unwrappedResponse = response as? [String: AnyObject], error == nil else {
+                return;
+            }
+            guard let urls = unwrappedResponse["network_authentication_target_urls"] as? [String] else {
+                return
+            }
+            ForcedHEManager.setHEUrls(Set<String>(urls))
+        });
     }
 
     func getBrowserTypeToUse() -> BrowserType {

TDConnectIosSdk/ForcedHEManager.h
@@ -8,8 +8,8 @@
 + (bool)isCellularEnabled;
 + (bool)shouldFetchThroughCellular:(NSString *)url;
 + (NSDictionary *) openUrlThroughCellular:(NSString *)url;
-+ (void)initForcedHE:(NSString *)wellKnownConfigurationEndpoint;
-+ (void)fetchWellknown:(NSString *)wellKnownConfigurationEndpoint completion:(void(^)(BOOL))completionHandler;
++ (void)initForcedHE;
++ (void)setHEUrls:(NSSet* )urlsForHE;
 @end
 
 #endif

TDConnectIosSdk/ForcedHEManager.m
@@ -15,47 +15,14 @@ int MAX_REDIRECTS_TO_FOLLOW_FOR_HE = 5;
 
 static NSSet *_urlsForHE = nil;
 
-+ (void)initForcedHE:(NSString *)wellKnownConfigurationEndpoint {
++ (void)initForcedHE {
     curl_global_init(CURL_GLOBAL_DEFAULT);
-
-    [self fetchWellknown:wellKnownConfigurationEndpoint completion:nil];
 }
 
-+ (void)fetchWellknown:(NSString *)wellKnownConfigurationEndpoint completion:(void(^)(BOOL))completionHandler {
-    NSURL *URL = [NSURL URLWithString:wellKnownConfigurationEndpoint];
-
-    __block NSDictionary *json;
-
-    NSURLSession *session = [NSURLSession sharedSession];
-    [[session dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                if (error || !data) {
-                    NSLog(@"Error fetching data from the endpoint %@", [error localizedDescription]);
-                    if (completionHandler) {
-                        completionHandler(false);
-                    }
-                    return;
-                }
-
-                NSError *serializationError = nil;
-                json = [NSJSONSerialization JSONObjectWithData:data
-                                                       options:0
-                                                         error:&serializationError];
-                if (serializationError) {
-                    NSLog(@"Error serializing the data %@", [serializationError localizedDescription]);
-                    if (completionHandler) {
-                        completionHandler(false);
-                    }
-                    return;
-                }
-
-                @synchronized(self) {
-                    _urlsForHE = json[@"network_authentication_target_urls"];
-                }
-
-                if (completionHandler) {
-                    completionHandler(true);
-                }
-    }] resume];
++ (void)setHEUrls:(NSSet *)urlsForHE {
+    @synchronized(self) {
+        _urlsForHE = urlsForHE;
+    }
 }
 
 + (bool)isInterfaceEnabled:(NSString *)iface {

commit d0444b337d02c21c22f6b313c80e725acb18d832

Fix broken tests
Changes to ForcedHEManagerTest.swift made in
eabfed97dfa7240733078c252ac9827f2b420637 broke some tests. This commit
removes some of these tests and corrects one change to make all tests pass.

We should consider adding back the tests removed in this commit, in a
correct manner.

TDConnectIosSdk.xcodeproj/project.pbxproj
@@ -42,7 +42,6 @@
 		5CF359B21CAA8C800064C43D /* IdTokenValidator.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5CF359B01CAA8C800064C43D /* IdTokenValidator.swift */; };
 		5CF359B31CAA8C800064C43D /* TelenorConnectOAuth2Module.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5CF359B11CAA8C800064C43D /* TelenorConnectOAuth2Module.swift */; };
 		6F1432B81A168594003BEE5B /* TDConnectIosSdk.framework in Copy Frameworks */ = {isa = PBXBuildFile; fileRef = 4833046219AF1635002F8DA9 /* TDConnectIosSdk.framework */; settings = {ATTRIBUTES = (CodeSignOnCopy, RemoveHeadersOnCopy, ); }; };
-		84D373F11FD38C060072C433 /* ForcedHEManagerTest.swift in Sources */ = {isa = PBXBuildFile; fileRef = 84D373F01FD38C060072C433 /* ForcedHEManagerTest.swift */; };
 		9F74D7BCCF0D94A87CEEB1FE /* Pods_TDConnectIosSdkTests.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 29A11A0B9B0A172620872B76 /* Pods_TDConnectIosSdkTests.framework */; };
 /* End PBXBuildFile section */
 
@@ -110,7 +109,6 @@
 		5C8F38A1205A72260008756F /* JsonResponseSerializerWithDate.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = JsonResponseSerializerWithDate.swift; sourceTree = "<group>"; };
 		5CF359B01CAA8C800064C43D /* IdTokenValidator.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = IdTokenValidator.swift; sourceTree = "<group>"; };
 		5CF359B11CAA8C800064C43D /* TelenorConnectOAuth2Module.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = TelenorConnectOAuth2Module.swift; sourceTree = "<group>"; };
-		84D373F01FD38C060072C433 /* ForcedHEManagerTest.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ForcedHEManagerTest.swift; sourceTree = "<group>"; };
 		9ADB5FF0CB048392A40B2D60 /* Pods.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods.framework; sourceTree = BUILT_PRODUCTS_DIR; };
 		A82EF108CA6E1CE1CB08AAE1 /* Pods-TDConnectIosSdk.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-TDConnectIosSdk.debug.xcconfig"; path = "Pods/Target Support Files/Pods-TDConnectIosSdk/Pods-TDConnectIosSdk.debug.xcconfig"; sourceTree = "<group>"; };
 		BE960CF64C41D83A722A3E4A /* Pods_TDConnectIosSdk.framework */ = {isa = PBXFileReference; explicitFileType = wrapper.framework; includeInIndex = 0; path = Pods_TDConnectIosSdk.framework; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -211,7 +209,6 @@
 				48432BE81A2F727C00F7A0FF /* ConfigTest.swift */,
 				5C542B571CABD3C400039789 /* IdTokenValidatorTest.swift */,
 				4833046F19AF1635002F8DA9 /* Supporting Files */,
-				84D373F01FD38C060072C433 /* ForcedHEManagerTest.swift */,
 			);
 			path = TDConnectIosSdkTests;
 			sourceTree = "<group>";
@@ -460,7 +457,6 @@
 				4872F71A19EFE80F00F3FDE8 /* DateUtilsTest.swift in Sources */,
 				5C542B581CABD3C400039789 /* IdTokenValidatorTest.swift in Sources */,
 				485D737B1A35B6240007D9EC /* OpenIDConnectFacebookOAuth2ModuleTest.swift in Sources */,
-				84D373F11FD38C060072C433 /* ForcedHEManagerTest.swift in Sources */,
 				485AAB941A383FB500ABEBB2 /* OAuth2ModuleMock.swift in Sources */,
 				48432BE91A2F727C00F7A0FF /* ConfigTest.swift in Sources */,
 				48C94D8019C1F0970000ABBB /* OAuth2ModuleTest.swift in Sources */,

TDConnectIosSdk.xcodeproj/xcshareddata/xcschemes/TDConnectIosSdk.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0940"
+   LastUpgradeVersion = "1000"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

TDConnectIosSdk/OAuth2Module.swift
@@ -150,7 +150,9 @@ open class OAuth2Module: NSObject, AuthzModule, SFSafariViewControllerDelegate {
         super.init()
 
         ForcedHEManager.initForcedHE()
-        self.fetchWellknownConfig()
+        if (config.wellKnownConfigurationEndpoint != nil) {
+            self.fetchWellknownConfig()
+        }
     }
 
     func fetchWellknownConfig() {

TDConnectIosSdk.xcodeproj/xcshareddata/xcschemes/TDConnectIosSdkTests.xcscheme
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
-   LastUpgradeVersion = "0940"
+   LastUpgradeVersion = "1000"
    version = "1.3">
    <BuildAction
       parallelizeBuildables = "YES"

TDConnectIosSdkTests/ForcedHEManagerTest.swift
@@ -1,83 +1,0 @@
-//
-//  ForcedHEManagerTest.swift
-//  TDConnectIosSdkTests
-//
-
-import Foundation
-import XCTest
-import TDConnectIosSdk
-import AeroGearHttp
-import OHHTTPStubs
-
-func setupForcedStubWithNSURLSessionDefaultConfiguration() {
-    // set up http stub
-    _ = stub({_ in return true}, response: { (request: URLRequest!) -> OHHTTPStubsResponse in
-        //_ = ["name": "John", "family_name": "Smith"]
-        switch request.url!.path {
-        case "/oauth/.well-known/openid-configuration":
-            let string = "{\"grant_types_supported\": [\"refresh_token\",\"authorization_code\"],\"id_token_signing_alg_values_supported\": [\"RS256\",\"none\"]}"
-            let data = string.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:data!, statusCode: 200, headers: ["Content-Type" : "text/json"])
-        case "/oauth/.well-known/openid-configuration-bad-url":
-            let string = ""
-            let data = string.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:data!, statusCode: 200, headers: ["Content-Type" : "text/json"])
-        default:
-            return OHHTTPStubsResponse(error: NSError(domain: "TimeoutErrorDomain", code: URLError.timedOut.rawValue, userInfo: nil))
-        }
-    })
-}
-
-class ForcedHEManagerTestModuleTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-        setupForcedStubWithNSURLSessionDefaultConfiguration()
-    }
-
-    override func tearDown() {
-        super.tearDown()
-        OHHTTPStubs.removeAllStubs()
-    }
-
-    func testGetRemoteConfiguration() {
-        let expectation = self.expectation(description: "GetRemoteConfiguration")
-        let baseUrl = "https://connect.staging.telenordigital.com/oauth"
-        let url = String(format: "%@", "\(baseUrl)/.well-known/openid-configuration")
-
-        ForcedHEManager.fetchWellknown(url) { (success) in
-            XCTAssert(success)
-            expectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 10, handler: nil)
-    }
-
-    func testGetRemoteConfigurationBadUrl() {
-        let expectation = self.expectation(description: "GetRemoteConfigurationBadUrl")
-        let baseUrl = "https://connect.staging.telenordigital.com/oauth"
-        let url = String(format: "%@", "\(baseUrl)/.well-known/openid-configuration-bad-url")
-
-        ForcedHEManager.fetchWellknown(url) { (success) in
-            XCTAssert(!success)
-            expectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 10, handler: nil)
-    }
-
-    func testGetRemoteConfigurationTimeout() {
-        let expectation = self.expectation(description: "GetRemoteConfigurationTimeout")
-        let baseUrl = "https://connect.staging.telenordigital.com/oauth"
-        let url = String(format: "%@", "\(baseUrl)")
-
-        ForcedHEManager.fetchWellknown(url) { (success) in
-            XCTAssert(!success)
-            expectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 10, handler: nil)
-    }
-
-}
-